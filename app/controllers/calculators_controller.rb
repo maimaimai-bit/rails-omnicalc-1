c6f084a0f10cd8b97506f9daf8a820da41042f30
maimaimai-bit <hmaivu02@gmail.com>
2025-02-11 14:45:21 -0600

    commit

---
+++ app/controllers/calculators_controller.rb
@@ -0,0 +1,38 @@
+class CalculatorsController < ApplicationController
+  def new_square
+  end
+
+  def square_results
+    @number = params[:number].to_f
+    @square = @number ** 2
+  end
+
+  def new_square_root
+  end
+
+  def square_root_results
+    @number = params[:number].to_f
+    @square_root = Math.sqrt(@number)
+  end
+
+  def new_payment
+  end
+
+  def payment_results
+    @apr = params[:apr].to_f
+    @years = params[:years].to_i
+    @principal = params[:principal].to_f
+    @formatted_apr = "%.4f%" % @apr
+    monthly_apr = @apr / 100 / 12
+    @monthly_payment = (@principal * monthly_apr) / (1 - (1 + monthly_apr) ** (-@years * 12))
+  end
+
+  def new_random
+  end
+
+  def random_results
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @random_number = rand(@min..@max)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get("/square/new", { :controller => "calculators", :action => "new_square" })
+  get("/square/results", { :controller => "calculators", :action => "square_results" })
   
+  get("/square_root/new", { :controller => "calculators", :action => "new_square_root" })
+  get("/square_root/results", { :controller => "calculators", :action => "square_root_results" })
+  
+  get("/payment/new", { :controller => "calculators", :action => "new_payment" })
+  get("/payment/results", { :controller => "calculators", :action => "payment_results" })
+  
+  get("/random/new", { :controller => "calculators", :action => "new_random" })
+  get("/random/results", { :controller => "calculators", :action => "random_results" })
 end
